03e4b96a3b2b598a2c8d12b08c18ada974dc3834
Inshaal Moin <moininshaal7@gmail.com>
2025-03-20 19:51:53 +0530

    testss

--- core/tests/test_snapshots.py
+++ core/tests/test_snapshots.py
@@ -6,6 +6,8 @@ from rest_framework.test import APIClient
 @pytest.mark.django_db
 def test_department_list(snapshot):
     client = APIClient()
+
+    
     url = reverse('department-list')
     response = client.get(url)
     assert response.status_code == 200
@@ -16,6 +18,7 @@ def test_department_list(snapshot):
 def test_course_list(snapshot):
     client = APIClient()
     url = reverse('course-list')
+
     response = client.get(url)
     assert response.status_code == 200
     
@@ -23,6 +26,7 @@ def test_course_list(snapshot):
 
 @pytest.mark.django_db
 def test_student_list(snapshot):
+
     client = APIClient()
     url = reverse('student-list')
     response = client.get(url)
